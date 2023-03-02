051a3496aa1e8d588c2761a9035317ed1a342508
JulianCamacho <jjulian.341@gmail.com>
2023-03-01 22:12:42 -0600

    Se agrega pantalla de gane con teclas

--- mobile-maze.asm
+++ mobile-maze.asm
@@ -13,12 +13,15 @@ mov es, ax ; ES:DI <-- B800:0000
 ;;============== CONSTANTES ==============
 ROWLEN      equ 160	; 80 Character row * 2 bytes each
 KEY_ENTER   equ 1Ch	; Keyboard scancodes...
+KEY_ESC     equ 01h	
+KEY_R       equ 13h
+
 
 
 
 ;;============== VARIABLES ==============
 drawColor: dw 0F0h
-
+vga_border: db '                                                                                ',0
 
 
 
@@ -61,11 +64,12 @@ initial_menu:
     cbw					; Zero out AH in 1 byte
     int 16h				; BIOS get keystroke, scancode in AH, character in AL
     cmp ah, KEY_ENTER	; Check what key user entered...
-    je game_loop        ; Go to game
+    je game_won        ; Go to game
 
 jmp initial_menu
 
 
+;;============= LOOP DEL JUEGO =============
 
 game_loop:
     ; Poner pantalla en color negro
@@ -76,7 +80,7 @@ game_loop:
 
     ; pintar el nombre
     mov si, nombreD
-    mov di, 1*2
+    mov di, ROWLEN*1+4
     call video_string
     
     ; pintar el nombre de Jason
@@ -85,6 +89,7 @@ game_loop:
 
     ;Pintado nivel    
     mov si, nivel
+    mov di, ROWLEN*2+2
     call video_string
 
     ;Pintando el nivel actual
@@ -92,6 +97,7 @@ game_loop:
     call video_string
         
     ;Pintando Obstaculos
+    mov di, ROWLEN*3+2
     mov si, obstaculos
     call video_string
 
@@ -100,7 +106,8 @@ game_loop:
     call video_string
 
 
-    ;Pintando los comandos de; juegos 
+    ;Pintando los comandos de; juegos
+    mov di, ROWLEN*4+2 
     mov si, comando
     call video_string
 
@@ -111,7 +118,58 @@ game_loop:
     .delay:
         cmp [0x046C], bx
         jl .delay
+
+
+     ;;if ganó: jmp game_won   
 jmp game_loop
+
+
+game_won:
+    ; Poner pantalla en color negro
+    xor ax, ax
+    xor di, di
+    mov cx, 80*25
+    rep stosw
+
+    mov si, welcome
+    mov di, ROWLEN*8+54    ;160 espacios*no.linea + offset
+    call video_string
+    
+    ; Delay
+    mov bx, [0x046C]
+    inc bx
+    inc bx
+    .delay:
+        cmp [0x046C], bx
+        jl .delay
+
+    get_player_input:
+		;; Get Player input
+		mov ah, 1			; BIOS get keyboard status int 16h AH 01h
+		int 16h
+		jz game_won		; No key entered, don't check, move on
+
+		cbw					; Zero out AH in 1 byte
+		int 16h				; BIOS get keystroke, scancode in AH, character in AL
+			
+		cmp ah, KEY_ESC		; Check what key user entered...
+		je esc_pressed
+		cmp ah, KEY_R
+		je r_pressed
+
+		jmp game_won		; Otherwise user entered some other key, move on, don't worry about it
+
+	;; Salir --> ir al menú inicial
+	esc_pressed:
+		jmp initial_menu
+
+	;; Reset game to initial state
+	r_pressed:
+        jmp game_loop   ;(?)
+		;int 19h			; Reloads the bootsector (in QEMU)
+
+jmp game_won
+
 
 
 
@@ -144,7 +202,6 @@ color_video_string:
 
            
 
-vga_border: db '                                                                                ',0
 welcome: db 'Bienvenido/a a Mobile Maze!', 0
 confirmation: db 'Presione ENTER para jugar!', 0
 nombreD: db 'Nombre: ', 0
@@ -153,6 +210,6 @@ nivel: db ' Nivel:',0
 nivelN: db '1',0
 obstaculos: db ' Obstaculos: ', 0
 obstaculosN: db '123', 0
-comando: db " Comandos:P,R,^,<,>,v", 0
+comando: db " Comandos: P,R,^,<,>,v", 0
 times 510 - ($ - $$) db 0       ; fill trainling zeros to get exactly 512 bytes long binary file
 dw 0xAA55                       ; set boot signature
